158f4b8f73664aa797a1db4981587ba858e640eb
Mukundraj Irkal <53365406+mukund2620@users.noreply.github.com>
2023-01-08 12:30:49 +0530

    Sign-functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { useState } from 'react';
 import { AiFillEyeInvisible,  AiFillEye} from "react-icons/ai"
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { signInWithEmailAndPassword, auth, getAuth } from 'firebase/auth';
+import {toast} from "react-toastify"
 
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false)
   const [formData, setFormData] = useState({email: "", password: ""});
 
   const {email, password} = formData
-
+  const Navigate = useNavigate()
   function onChange(e){
     setFormData((prevState) => ({
       ...prevState,
@@ -17,6 +19,21 @@ export default function SignIn() {
 
       
     }))
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      const userCredentials = await signInWithEmailAndPassword(auth, email, password)
+      if(userCredentials.user){
+        Navigate("/")
+      }
+      
+    } catch (error) {
+      toast.error("Bad user credentials")
+      
+    }
   }
   return (
    <section>
@@ -27,7 +44,7 @@ export default function SignIn() {
       </div>
 
       <div  className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-        <form>
+        <form onSubmit={onSubmit()}>
           <input className=" mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out" type="email" id="email" value={email} onChange={onChange} placeholder="Email Address" />
           <div className='relative mb-6'>
           <input className=" w-full  px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out" type={showPassword ? "text" : "password"} id="password" value={password} onChange={onChange} placeholder="Password" />
