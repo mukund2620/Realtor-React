8b6029586236ef3c5fa7f6e68d63936460d2aa4d
Mukundraj Irkal <53365406+mukund2620@users.noreply.github.com>
2023-02-02 11:23:57 +0530

    Dynamic Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
-
+import {getAuth, onAuthStateChanged} from "firebase/auth"
+import { useEffect } from 'react'
 export default function Header() {
+
+    const [pageState, setpageState] = useState("Sign In")
 
     const location = useLocation()
     function pathMatchRoute(route){
@@ -11,6 +14,20 @@ export default function Header() {
     }
 
     const navigate = useNavigate()
+
+    const auth = getAuth();
+   useEffect(() => {
+
+    onAuthStateChanged(auth, (user)=> {
+        if(user){
+            setpageState("Profile")
+        }
+        else{
+            setpageState("Sign In")
+        }
+    })
+
+   }, [auth])
 
   return (
     <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
@@ -23,7 +40,7 @@ export default function Header() {
                 <ul className='flex space-x-10'>
                     <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-black border-b-blue-700"}` } onClick={()=> navigate("/")}>Home</li>
                     <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/offers") && "text-black border-b-blue-700"}`} onClick={()=> navigate("/offers")}>Offers</li>
-                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/sign-in") && "text-black border-b-blue-700"}`} onClick={()=> navigate("/sign-in")}>Sign In</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${(pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) && "text-black border-b-red-500"}`} onClick={()=> navigate("/profile")}>{pageState}</li>
                 </ul>
             </div>
         </header>
